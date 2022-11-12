197c4180e347bf53b295e3b83dd6a4b56d24839f
Rajin Sharwar <rajinsharwar@gmail.com>
2022-11-12 19:00:08 +0600

    Updating the main file

--- smoothlinks.php
+++ smoothlinks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: SmoothLinks
- * Description: A super light-weight WordPress plugin to automatically open the links sent as a private message in a new tab.Currently this plugin only supports the private messaging of BuddyBoss and BuddyPress. No configurations needed. Just activate it, and your forums will have the breadcrumbs on it.
+ * Description: A super light-weight WordPress plugin to automatically open the links sent as a private message in a new tab. Currently this plugin only supports the private messaging of BuddyBoss and BuddyPress. No configurations needed. Just activate it, and you are good to go.
  * Author: Rajin Sharwar
  * Author URI: https://linkedin.com/in/rajinsharwar
  * Version: 1.0.0
