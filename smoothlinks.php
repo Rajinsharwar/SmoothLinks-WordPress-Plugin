05224b32efa890fb757623d9b24bff7de26a4686
Rajin Sharwar <rajinsharwar@gmail.com>
2022-11-12 18:57:34 +0600

    Plugin's Initial commit

---
+++ smoothlinks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: SmoothLinks
+ * Description: A super light-weight WordPress plugin to automatically open the links sent as a private message in a new tab.Currently this plugin only supports the private messaging of BuddyBoss and BuddyPress. No configurations needed. Just activate it, and your forums will have the breadcrumbs on it.
+ * Author: Rajin Sharwar
+ * Author URI: https://linkedin.com/in/rajinsharwar
+ * Version: 1.0.0
+ * Text Domain: smoothlinks
+ */
+
+add_filter( 'bp_get_the_thread_message_content', 'bp_message_link_clickable_with_target', 99 ); 
+function bp_message_link_clickable_with_target( $message ) {
+     $message = preg_replace( '/<a /','<a target="_blank" ', make_clickable( $message ) );
+return $message;
+}
